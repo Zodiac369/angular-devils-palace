4a5d8ea6ce5e21fe0260a9003955fd2e61c1ee48
3MRE <emre.ogk@hotmail.fr>
2024-02-01 21:21:55 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo:'dashboard', pathMatch:'full'
+  },
+  {
+    path: 'home', component: DashboardComponent
+  },
+  {
+    path: 'films', component: ProductsComponent
+  },
+  {
+    path: 'top', component: StatisticsComponent
+  },
+  {
+    path: 'genres', component: CoupensComponent
+  },
+  {
+    path: 'profil', component: PagesComponent
+  },
+  {
+    path: 'media', component: MediaComponent
+  },
+  {
+    path: 'settings', component: SettingsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,39 @@
+import { RouterLink } from "@angular/router";
+
+export const navbarData = [
+    {
+        routerLink: 'home',
+        icon: 'fal fa-home',
+        label: 'Home'
+    },
+    {
+        routerLink: 'films',
+        icon: 'fal fa-box-open',
+        label: 'Films'
+    },
+    {
+        routerLink: 'top',
+        icon: 'fal fa-chart-bar',
+        label: 'Top'
+    },
+    {
+        routerLink: 'genres',
+        icon: 'fal fa-tags',
+        label: 'Genres'
+    },
+    {
+        routerLink: 'profil',
+        icon: 'fal fa-file',
+        label: 'Profil'
+    },
+    {
+        routerLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Social'
+    },
+    {
+        routerLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Paramètres'
+    }
+];
